8b92e1bddaa48b979c3d0853275ac088c2559818
Zainab Lawal <zainablaw1012@gmail.com>
2023-05-08 15:23:00 +0300

    Store only previous and current reorderPoints instead of full lists

--- src/main/java/OLCalculator.java
+++ src/main/java/OLCalculator.java
@@ -20,7 +20,7 @@ public class OLCalculator {
 		double interestRate = scanner.nextDouble();
 
 		//Calculate holding cost
-		double holdingCost = (unitCost * interestRate)/100;
+		double holdingCost = (unitCost * interestRate) / 100;
 
 		// Next request lead time, lead time demand and lead time standard
 		System.out.println("Enter lead time: ");
@@ -32,63 +32,57 @@ public class OLCalculator {
 		System.out.println("Enter lead time standard Deviation: ");
 		double leadTimeSD = scanner.nextDouble();
 
-		double annualDemand=(12/leadTime)*leadTimeDemand;
+		double annualDemand = (12 / leadTime) * leadTimeDemand;
 
-		//list of reorder points
-		List<Integer> reorderPoints = new ArrayList<>();
-		List<Double> lot_size= new ArrayList<>();
-		List<Double> f_r= new ArrayList<>();
-		int count=-1;
+		int reorderPoint = 0;
+		double prevReorderPoint = 0;
+
+		double lotSize = Math.sqrt((2 * orderingCost * annualDemand) / holdingCost);
+
+		// Request z and L(z) values
+		int i = 0;
+		int count = 0;
 		double z;
 		double l_z;
-		double n_r;//n(R)
-		double Q0=Math.sqrt((2*orderingCost*annualDemand)/holdingCost);
-
-		lot_size.add(Q0);
-
-		//Request z and L(z) values
-		int i =-1;
-		do{
-			i++;
-
-			f_r.add((1-((lot_size.get(i)*holdingCost))/(penaltyCost*annualDemand)));
-
-			System.out.println("Enter z values according to "+ f_r.get(i)+ " value from the table.");
-			z= scanner.nextDouble();
-			reorderPoints.add((int) (leadTimeDemand +(leadTimeSD*z)));
-			System.out.println(reorderPoints.get(i));
-			System.out.println("Enter L(z) values for z " +z);
-			l_z=scanner.nextDouble();
-			n_r=leadTimeSD*l_z;
+		double n_r; //n(R)
+		double Q0 = lotSize;
+		do {
+			prevReorderPoint = reorderPoint;
+			double f_r = (1 - (lotSize * holdingCost) / (penaltyCost * annualDemand));
+			System.out.println("Enter z values according to " + f_r + " value from the table.");
+			z = scanner.nextDouble();
+			reorderPoint = (int) (leadTimeDemand + (leadTimeSD * z));
+			System.out.println(reorderPoint);
+			System.out.println("Enter L(z) values for z " + z);
+			l_z = scanner.nextDouble();
+			n_r = leadTimeSD * l_z;
 			System.out.println(n_r);
-			lot_size.add(Math.sqrt((2*annualDemand*(orderingCost+(penaltyCost*n_r)))/(holdingCost)));
-			System.out.println(lot_size.get(i));
+			lotSize = Math.sqrt((2 * annualDemand * (orderingCost + (penaltyCost * n_r))) / (holdingCost));
+			System.out.println(lotSize);
 			count++;
+			i++;
+		} while (i == 1 || (Math.abs(reorderPoint - prevReorderPoint) > (reorderPoint / 100)));
 
+		scanner.close();
 
-		} while(
-				i == 0 || ((Math.abs(reorderPoints.get(i)-reorderPoints.get(i - 1))>(reorderPoints.get(i)/100)))
-		);
-
-
-		System.out.println("Optimal lot size and reorder point (Q,R) " + lot_size.get(lot_size.size()-1)+" ; "+ reorderPoints.get(reorderPoints.size()-1));
+		System.out.println("Optimal lot size and reorder point (Q,R) " + lotSize + " : " + reorderPoint);
 		System.out.println();
 		System.out.println("Number of iterations : " + count);
 		System.out.println();
-		System.out.println("The safety stock : " + (reorderPoints.get(reorderPoints.size()-1)-leadTimeDemand));
+		System.out.println("The safety stock : " + (reorderPoint - leadTimeDemand));
 		System.out.println();
-		System.out.println("Avarage annual holding cost : " +(holdingCost*((lot_size.get(lot_size.size()-1)/2)+reorderPoints.get(reorderPoints.size()-1)-leadTimeDemand)) );
-		System.out.println("Avarage annual ordering cost : " +(orderingCost*annualDemand)/lot_size.get(lot_size.size()-1));
-		System.out.println("Avarage annual penalty cost : " + (penaltyCost*annualDemand*n_r)/lot_size.get(lot_size.size()-1));
+		System.out.println("Average annual holding cost : " + (holdingCost * ((lotSize / 2) + reorderPoint - leadTimeDemand)));
+		System.out.println("Average annual ordering cost : " + (orderingCost * annualDemand) / lotSize);
+		System.out.println("Average annual penalty cost : " + (penaltyCost * annualDemand * n_r) / lotSize);
 		System.out.println();
-		System.out.println("Average time between the placement of orders : " + lot_size.get(lot_size.size()-1)/annualDemand);
+		System.out.println("Average time between the placement of orders : " + lotSize / annualDemand);
 		System.out.println();
-		System.out.print("The proportion of order cycles in which no stock out occurs" + f_r.get(f_r.size()-1) +" means %"+ (f_r.get(f_r.size()-1))*100);
+		System.out.println("The proportion of order cycles in which no stock out occurs " + (1 - (lotSize * holdingCost) / (penaltyCost * annualDemand)) * 100 + "%");
 		System.out.println();
 		System.out.println("The proportion of demands that are not met");
-		System.out.println("Expected Expected demand per cycle is Q = "+ (lot_size.get(lot_size.size()-1)+1));
-		System.out.println("Expected number of stock outs per cycle is n(R) = "+ n_r);
-		System.out.println("Thus, proportion of demands that are not met is " + "2,01/291 = " +  (n_r/(lot_size.get(lot_size.size()-1))));
+		System.out.println("Expected Expected demand per cycle is Q = " + (lotSize + 1));
+		System.out.println("Expected number of stock outs per cycle is n(R) = " + n_r);
+		System.out.println("Thus, proportion of demands that are not met is " + "= " + ((n_r/lotSize) * 100));
 		scanner.close();
 	}
 }
